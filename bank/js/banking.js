0796e4575e8d372006c489d6ec3b918d6f58c66f
limonzishad <zishadlimon@gmail.com>
2022-02-14 00:52:10 +0600

    deposit, withdraw and balance calculation

--- bank/js/banking.js
+++ bank/js/banking.js
@@ -1,10 +1,26 @@
 document.getElementById('deposit').addEventListener('click', function () {
     const depositField = document.getElementById('deposit-money');
     const depositAmount = depositField.value;
-    console.log(depositAmount);
+    const depositTotal = document.getElementById('deposit-total');
+    const previousDepositTotal = depositTotal.innerText;
+    const updatedDepositTotal = parseFloat(depositAmount) + parseFloat(previousDepositTotal);
+    depositTotal.innerText = updatedDepositTotal;
+    depositField.value = '';
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotal = balanceTotal.innerText;
+    const updatedBalanceTotal = parseFloat(previousBalanceTotal) + parseFloat(depositAmount);
+    balanceTotal.innerText = updatedBalanceTotal;
 });
 document.getElementById('withdraw').addEventListener('click', function () {
     const withdrawField = document.getElementById('withdraw-money');
     const withdrawAmount = withdrawField.value;
-    console.log(withdrawAmount);
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawTotal = withdrawTotal.innerText;
+    const updatedWithdrawTotal = parseFloat(withdrawAmount) + parseFloat(previousWithdrawTotal);
+    withdrawTotal.innerText = updatedWithdrawTotal;
+    withdrawField.value = '';
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotal = balanceTotal.innerText;
+    const updatedBalanceTotal = parseFloat(previousBalanceTotal) - parseFloat(withdrawAmount);
+    balanceTotal.innerText = updatedBalanceTotal;
 });
